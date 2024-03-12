f7e6bd725dd2290bcfc5a1707f85e2344fecca79
Manuel Meraz <manuelmeraz@Manuels-MacBook-Air.local>
2024-03-11 19:06:29 -0500

    redux changes

--- frontend/src/components/UserSearchBar.js
+++ frontend/src/components/UserSearchBar.js
@@ -1,8 +1,28 @@
 import React , { useState }from 'react'
-
+ //hiiiii
 
 const UserSearchBar = ({data, onSearch}) => {
     const [searchTerm, setSearchTerm] = useState('')
 
+    const handleSearchChange = (e) => {
+        const term = e.target.value
+        setSearchTerm(term)
+        onSearch(term)
+    }
+
+    return(
+        <div>
+            <label htmlFor='search'>Search:</label>
+            <input
+                type = "text"
+                id = "search"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder='Type to search...'
+                />
+        </div>
+    );
 
 }
+
+export default UserSearchBar;
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,14 +1,39 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import UserSearchBar from "../components/UserSearchBar";
+import userService from "../services/users";
+import { useState, useEffect } from 'react';
+
 
 const Landing = () => {
+  const[users, setUsers] = useState([])
+  const[filteredItems, setFilteredItems] = useState([])
 
   const user = useSelector((state) => state.user.value)
+
+
+  const hook = () => {
+    console.log('effect')
+    userService.getAll().then(initialArr => setUsers(initialArr))   //initialArr is what we get from noteservice obj (array), we then set it to persons
+  }
+
+  useEffect(hook, [])
+
+  const handleSearch = (term) =>{
+    const filtered = users.filter((item) => item.toLowerCase().includes(term.toLowerCase()))
+    setFilteredItems(filtered)
+  }
+
+
   return (
     <div>
       <h1>Home Page</h1>
       <div>Hi {user.name} !</div>
       <div>{user.username} !</div>
+      <UserSearchBar data = {users} onSearch={handleSearch}/>
+      <ul>
+        {filteredItems.map((item, index) => (<li key={index}>{item}</li>))}
+      </ul>
     </div>
   );
 };
