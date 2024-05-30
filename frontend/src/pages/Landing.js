8f0c6ca4f24dca2db675ed894340e1167322330a
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-05-30 01:44:45 -0500

    friends listtest

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setGlobalUser  } from '../store'
 import FriendList from "../components/FriendList";
 
-//comment
+//commentfwhegw
 
 
 const Landing = () => {
